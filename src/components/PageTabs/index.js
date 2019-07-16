241513f061d96b98157e1e07fd7e30276b63a2dc
xieyunsheng <xieyunsheng>
2019-07-16 12:12:50 +0800

    fix: searchPathIdAndName to compatible with subroute and dynamic route

--- src/components/PageTabs/index.js
+++ src/components/PageTabs/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import pathToRegexp from 'path-to-regexp';
-import memoizeOne from 'memoize-one';
-import isEqual from 'lodash/isEqual';
 import _find from 'lodash/find';
 import router from 'umi/router';
 
 import ChildrenTabs from '@/components/ChildrenTabs';
-import { flatTreeToList } from '@/utils/treeUtils'; // 引入工具函数
 
 function getChildrenPathname(children) {
   const {
@@ -17,40 +14,23 @@ function getChildrenPathname(children) {
   return childrenPathname;
 }
 
-function flatMenuTreeToList(menu) {
-  function nodeTransfer(node) {
-    return {
-      path: node.path,
-      name: node.name,
-      locale: node.locale,
-      // content: node.component,
-    };
-  }
-  return flatTreeToList(menu, {
-    nodeTransfer,
-  });
-}
-
-const memoizeOneFlatMenuTreeToList = memoizeOne(flatMenuTreeToList, isEqual);
-
 function searchPathIdAndName(childrenPathname, originalMenuData) {
-  let result = ['404', 'Error'];
-  let pathToSearchPathId = childrenPathname;
-  const routeDepthInMenu = 2;
-  const pathSegments = childrenPathname.split(/\//g).filter(item => item);
-  if (pathSegments.length > routeDepthInMenu) {
-    pathToSearchPathId = `/${pathSegments.slice(0, routeDepthInMenu).join('/')}`;
-  }
-  // console.log(pathSegments, pathToSearchPathId);
+  const getPathIdAndName = (path, menuData) => {
+    let result;
+    menuData.forEach(item => {
+      // match prefix
+      if (pathToRegexp(`${item.path}(.*)`).test(path)) {
+        result = [item.path, item.name];
 
-  const flatedMenu = memoizeOneFlatMenuTreeToList(originalMenuData);
-  for (let i = 0; i < flatedMenu.length; i += 1) {
-    if (pathToRegexp(flatedMenu[i].path).test(pathToSearchPathId)) {
-      result = [flatedMenu[i].path, flatedMenu[i].name];
-      break;
-    }
-  }
-  return result;
+        // get children authority recursively
+        if (item.children) {
+          result = searchPathIdAndName(path, item.children) || result;
+        }
+      }
+    });
+    return result;
+  };
+  return getPathIdAndName(childrenPathname, originalMenuData) || ['404', 'Error'];
 }
 
 export default function PageTabs(props) {
