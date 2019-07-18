89c25c17670e860a38e8dd78f27d0b93d386d267
Yuns Xie <yuns.xie@qq.com>
2019-07-18 21:49:31 +0800

    feat: add rowProps and remove useless code

--- src/components/QueryPanel/index.js
+++ src/components/QueryPanel/index.js
@@ -5,8 +5,6 @@ import { FormProvider, createFormItems } from '@/components/antd-form-pro';
 import { callFunctionIfFunction } from '@/utils/decorators/callFunctionOrNot';
 import styles from './index.less';
 
-const SIMPLE_COUNT = 2;
-
 const addAllowClearToItemsConfig = itemsConfig =>
   itemsConfig.map(item => ({ ...item, allowClear: true }));
 
@@ -41,83 +39,13 @@ class QueryPanel extends PureComponent {
     });
   };
 
-  renderSimpleForm() {
-    const { form, queryArgsConfig = [] } = this.props;
-    return (
-      <Form onSubmit={this.handleSubmit} layout="inline">
-        <Row gutter={{ md: 8, lg: 24, xl: 48 }}>
-          <FormProvider value={form}>
-            {createFormItems(
-              addAllowClearToItemsConfig(queryArgsConfig).slice(0, SIMPLE_COUNT)
-            ).map(item => (
-              <Col key={item.key} md={8} sm={24}>
-                {item}
-              </Col>
-            ))}
-          </FormProvider>
-          <Col md={8} sm={24}>
-            <span className={styles.submitButtons}>
-              <Button type="primary" htmlType="submit">
-                查询
-              </Button>
-              <Button style={{ marginLeft: 8 }} onClick={this.handleFormReset}>
-                重置
-              </Button>
-              {queryArgsConfig.length <= SIMPLE_COUNT ? null : (
-                <a style={{ marginLeft: 8 }} onClick={this.toggleForm}>
-                  展开 <Icon type="down" />
-                </a>
-              )}
-            </span>
-          </Col>
-        </Row>
-      </Form>
-    );
-  }
-
-  renderAdvancedForm() {
-    const { form, queryArgsConfig = [] } = this.props;
-    return (
-      <Form onSubmit={this.handleSubmit} layout="inline">
-        <FormProvider value={form}>
-          <Row gutter={{ md: 8, lg: 24, xl: 48 }}>
-            {createFormItems(addAllowClearToItemsConfig(queryArgsConfig).slice(0, 3)).map(item => (
-              <Col key={item.key} md={8} sm={24}>
-                {item}
-              </Col>
-            ))}
-          </Row>
-          <Row gutter={{ md: 8, lg: 24, xl: 48 }}>
-            {createFormItems(addAllowClearToItemsConfig(queryArgsConfig).slice(3, 6)).map(item => (
-              <Col key={item.key} md={8} sm={24}>
-                {item}
-              </Col>
-            ))}
-          </Row>
-        </FormProvider>
-        <div style={{ overflow: 'hidden' }}>
-          <div style={{ marginBottom: 24 }}>
-            <Button type="primary" htmlType="submit">
-              查询
-            </Button>
-            <Button style={{ marginLeft: 8 }} onClick={this.handleFormReset}>
-              重置
-            </Button>
-            <a style={{ marginLeft: 8 }} onClick={this.toggleForm}>
-              收起 <Icon type="up" />
-            </a>
-          </div>
-        </div>
-      </Form>
-    );
-  }
-
   renderForm() {
     const {
       form,
       queryArgsConfig = [],
       rowCount = 3,
       maxCount = 2,
+      rowProps = {},
       colProps: customColProps,
     } = this.props;
     let colProps = { span: 24 / rowCount };
@@ -159,7 +87,7 @@ class QueryPanel extends PureComponent {
     return (
       <Form onSubmit={this.handleSubmit} layout="inline">
         <FormProvider value={form}>
-          <Row gutter={{ md: 8, lg: 24, xl: 48 }}>
+          <Row gutter={{ md: 8, lg: 24, xl: 48 }} {...rowProps}>
             {formItems.map(item => (
               <Col {...colProps} key={item.key}>
                 {item}
