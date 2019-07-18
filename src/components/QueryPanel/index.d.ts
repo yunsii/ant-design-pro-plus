9d514f01c12d571f6234adb106062e8f74f15f4a
Yuns Xie <yuns.xie@qq.com>
2019-07-18 21:50:34 +0800

    feat: update api desc

--- src/components/QueryPanel/index.d.ts
+++ src/components/QueryPanel/index.d.ts
@@ -1,7 +1,17 @@
 import * as React from 'react';
+import { RowProps } from 'antd/lib/row';
+import { ColProps } from 'antd/lib/col';
 
-export declare type QueryPanelProps = {};
-interface QueryPanelState {}
+export declare type QueryPanelProps = {
+  queryArgsConfig: any[];
+  rowCount: number;
+  maxCount: number;
+  rowProps: RowProps;
+  colProps: ColProps;
+};
+interface QueryPanelState {
+  expandForm: boolean;
+}
 
 declare class QueryPanel extends React.Component<QueryPanelProps, QueryPanelState> {}
 export default QueryPanel;
