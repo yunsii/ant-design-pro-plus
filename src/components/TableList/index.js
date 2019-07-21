064f79e41179df30e35b8e0e0a6d18eea565ff69
Yuns Xie <yuns.xie@qq.com>
2019-07-21 17:45:05 +0800

    feat: handle pagination change

--- src/components/TableList/index.js
+++ src/components/TableList/index.js
@@ -1,22 +1,47 @@
 import React, { PureComponent, Fragment } from 'react';
-import { List, Alert, Pagination, Checkbox } from 'antd';
+import { List, Alert, Pagination, Checkbox, Spin } from 'antd';
+import _isEqual from 'lodash/isEqual';
 // import classNames from 'classnames';
 import styles from './index.less';
 
 class TableList extends PureComponent {
-  static getDerivedStateFromProps(nextProps) {
-    // clean state
-    const { checkable } = nextProps;
+  static getDerivedStateFromProps(nextProps, state) {
+    let result = null;
+    const { checkable, data = {} } = nextProps;
+    const { propsPaination } = state;
+    const { pagination, list } = data;
+    console.log(pagination, propsPaination);
+    console.log(!pagination && !propsPaination);
+    console.log(pagination && !_isEqual(propsPaination, pagination));
+    if (!pagination && !propsPaination) {
+      result = {
+        propsPaination: pagination,
+        pagination: {
+          current: 1,
+          pageSize: 10,
+          total: list.length,
+        },
+      };
+    }
+    if (pagination && !_isEqual(propsPaination, pagination)) {
+      result = {
+        propsPaination: pagination,
+        pagination,
+      };
+    }
     if (checkable && nextProps.selectedRows.length === 0) {
-      return {
+      result = {
+        ...result,
         selectedRowKeys: [],
       };
     }
-    return null;
+    return result;
   }
 
   state = {
     selectedRowKeys: [],
+    propsPaination: null,
+    pagination: {},
   };
 
   handleTableChange = (pagination, filters, sorter) => {
@@ -52,8 +77,28 @@ class TableList extends PureComponent {
     }
   };
 
+  handlePageChange = (current, pageSize) => {
+    const { onChange } = this.props;
+    const { pagination } = this.state;
+    // console.log(pagination);
+    this.setState({
+      pagination: {
+        current,
+        pageSize,
+        ...pagination,
+      },
+    });
+    onChange({
+      current,
+      pageSize,
+    });
+  };
+
+  handleShowSizeChange = (current, pageSize) => {
+    this.handlePageChange(current, pageSize);
+  };
+
   render() {
-    const { selectedRowKeys } = this.state;
     const {
       data = {},
       rowKey,
@@ -61,9 +106,12 @@ class TableList extends PureComponent {
       renderItem,
       selectedRows,
       setActions,
+      onSelectRow,
+      loading,
       ...rest
     } = this.props;
-    const { list = [], pagination } = data;
+    const { selectedRowKeys, pagination } = this.state;
+    const { list = [] } = data;
 
     const paginationProps = {
       showSizeChanger: true,
@@ -72,6 +120,7 @@ class TableList extends PureComponent {
       showTotal: (total, range) => `${range[0]}-${range[1]}，总计 ${total} 条`,
       ...pagination,
     };
+    console.log(pagination);
 
     let recordSelection = {
       selectedRowKeys,
@@ -85,29 +134,37 @@ class TableList extends PureComponent {
     }
 
     let renderList = (
-      <List
-        rowKey="id"
-        grid={{ gutter: 24, lg: 3, md: 2, sm: 1, xs: 1 }}
-        {...rest}
-        dataSource={list}
-        renderItem={record => (
-          <List.Item style={{ position: 'relative' }}>
-            {checkable ? (
-              <Checkbox
-                value={record.id}
-                className={styles.checkBox}
-                style={{
-                  position: 'absolute',
-                  zIndex: 1,
-                  top: 5,
-                  right: 4,
-                }}
-              />
-            ) : null}
-            {renderItem({ record, actions: setActions(record), recordSelection, checkable })}
-          </List.Item>
-        )}
-      />
+      <Spin spinning={loading}>
+        <List
+          rowKey="id"
+          grid={{ gutter: 24, lg: 3, md: 2, sm: 1, xs: 1 }}
+          {...rest}
+          dataSource={list}
+          renderItem={record => (
+            <List.Item style={{ position: 'relative' }}>
+              {checkable ? (
+                <Checkbox
+                  value={record.id}
+                  className={styles.checkBox}
+                  style={{
+                    position: 'absolute',
+                    zIndex: 1,
+                    top: 5,
+                    right: 4,
+                  }}
+                />
+              ) : null}
+              {renderItem({ record, actions: setActions(record), recordSelection, checkable })}
+            </List.Item>
+          )}
+        />
+        <Pagination
+          style={{ float: 'right' }}
+          {...paginationProps}
+          onChange={this.handlePageChange}
+          onShowSizeChange={this.handleShowSizeChange}
+        />
+      </Spin>
     );
     if (checkable) {
       renderList = (
@@ -142,7 +199,6 @@ class TableList extends PureComponent {
           </div>
         ) : null}
         {renderList}
-        <Pagination {...paginationProps} />
       </div>
     );
   }
