acfad264a0803afd1d94e619c831bab8196a5409
xieyunsheng <xieyunsheng>
2019-07-22 18:39:12 +0800

    feat: add renderChildren

---
+++ src/utils/childrenUtils.ts
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function renderChildren(children, properties) {
+  return React.Children.map(children, child => {
+    if (child) {
+      const { type: childType } = child;
+      if (typeof childType === 'string') {
+        return child;
+      }
+      return React.cloneElement(child, {
+        ...properties,
+      });
+    }
+    return child;
+  });
+}
