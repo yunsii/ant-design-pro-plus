83c6ec5b7f3130204c82210ed724d144e1d781dc
Yuns Xie <yuns.xie@qq.com>
2019-07-29 21:50:44 +0800

    feat: set datetime by moment

--- src/components/antd-form-pro/components/CustomDatePicker/index.js
+++ src/components/antd-form-pro/components/CustomDatePicker/index.js
@@ -1,15 +1,32 @@
 /* eslint-disable react/destructuring-assignment */
 import React, { Component } from 'react';
 import moment from 'moment';
+import _isArray from 'lodash/isArray';
 import { DatePicker } from 'antd';
 
 // Warning: Function components cannot be given refs.
 const { RangePicker } = DatePicker;
 
+function setDataPickerValue(value) {
+  if (!value) return null;
+  if (value instanceof moment) return value;
+  return moment(value);
+}
+
+function setRangePickerValue(value) {
+  if (!value) return null;
+  if (!(_isArray(value) && value.length >= 2)) {
+    console.error('RangePicker value is error:', value);
+    return null;
+  }
+  if (value[0] instanceof moment) return value;
+  return [moment(value[0]), moment(value[1])];
+}
+
 export class CustomRangePicker extends Component {
   handleChange = (dates, dateStrings) => {
     if (this.props.onChange) {
-      this.props.onChange(dateStrings);
+      this.props.onChange(dates, dateStrings);
     }
   };
 
@@ -18,7 +35,7 @@ export class CustomRangePicker extends Component {
 
     return (
       <RangePicker
-        value={!value ? null : [moment(value[0]), moment(value[1])]}
+        value={setRangePickerValue(value)}
         allowClear={false}
         onChange={this.handleChange}
         {...rest}
@@ -31,7 +48,7 @@ export default class CustomDatePicker extends Component {
   handleChange = (timeMoment, timeString) => {
     // console.log(timeMoment, timeString);  // null ""
     if (this.props.onChange) {
-      this.props.onChange(timeString);
+      this.props.onChange(timeMoment, timeString);
     }
   };
 
@@ -39,7 +56,7 @@ export default class CustomDatePicker extends Component {
     const { onChange, value, ...rest } = this.props;
     return (
       <DatePicker
-        value={!value ? null : moment(value)}
+        value={setDataPickerValue(value)}
         allowClear={false}
         onChange={this.handleChange}
         {...rest}
