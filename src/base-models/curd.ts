400004a4811cb7f48da08f4cf7ecdab174f797f8
xieyunsheng <xieyunsheng>
2019-08-01 10:33:20 +0800

    feat: update dva-base-models

--- src/base-models/curd.ts
+++ src/base-models/curd.ts
@@ -1,10 +1,10 @@
 import { curdModel } from 'dva-base-models';
 import { getTableList, getData, isResponseOk } from '@/utils/model';
 
-curdModel.curdModelConfig({
+curdModel.config({
   getTableList,
   getData,
   isResponseOk,
 })
 
-export default curdModel.setCurdModel;
+export default curdModel.set;
