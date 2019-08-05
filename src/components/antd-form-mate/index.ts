29e54f0d17b04a66b8259b4f56531f9bedb27691
Yuns Xie <yuns.xie@qq.com>
2019-08-05 22:58:24 +0800

    chore: udate antd-form-mate

--- src/components/antd-form-mate/index.ts
+++ src/components/antd-form-mate/index.ts
@@ -10,7 +10,7 @@ uploadConfig({
 
 const { setDefaultExtra } = FormMate;
 setDefaultExtra({
-  picture: '自定义图片默认提示'
-})
+  picture: '自定义图片默认提示',
+});
 
 export const { FormProvider, createFormItems } = FormMate;
