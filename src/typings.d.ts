cc95f506b47a1b5669be6ce73f66b4a46a29823c
Yuns Xie <yuns.xie@qq.com>
2019-08-15 23:04:17 +0800

    feat: type declaration

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,1 +1,16 @@
 declare module '*.less';
+
+declare interface UmiChildren extends React.ReactChildren {
+  props: {
+    location: Location;
+  };
+}
+
+declare interface MenuItem {
+  authority: string[] | undefined;
+  children?: UmiChildren[];
+  component?: React.Component;
+  icons?: string;
+  name?: string;
+  path: string;
+}
