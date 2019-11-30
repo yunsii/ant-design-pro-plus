c1f516056bb771845562dcad1927681baf1d1d00
Yuns Xie <yuns.xie@qq.com>
2019-11-30 14:11:07 +0800

    refactor: pageTabsMode

--- src/defaultSettings.js
+++ src/defaultSettings.js
@@ -1,3 +1,10 @@
+// 标签页功能配置
+const pageTabsMode = [
+  'route', // 使用页面路由定义作为标签页 id ，类似 /path/:name 这样的动态路由只会打开一个标签页
+  'path', // 使用页面路径作为标签页 id ，动态路由会打开不同的标签页，因此，可能需要再在 PageTabs 组件中动态设置标签页的标题
+  false, // 不使用标签页功能
+];
+
 module.exports = {
   navTheme: 'dark', // theme for nav menu
   primaryColor: '#1890FF', // primary color of ant design
@@ -17,7 +24,7 @@ module.exports = {
   // Usage: https://github.com/ant-design/ant-design-pro/pull/3517
   iconfontUrl: '',
 
-  pageTabs: true,
+  pageTabs: pageTabsMode[0],
   proRootPath: '/',
   reloadTab: true,
 };
