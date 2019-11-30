e3d37f6a9fef21cc887e0ea60f3fbfdecb67c151
Yuns Xie <yuns.xie@qq.com>
2019-11-30 14:07:35 +0800

    feat: pageTabs mode and tabsConfig

--- src/components/PageTabs/index.tsx
+++ src/components/PageTabs/index.tsx
@@ -32,26 +32,49 @@ function getMetadataOfTab(
   return getMetadata(childrenPathname, originalMenuData, null) || ['404', 'Error'];
 }
 
+/**
+ * 如果是根据路径来展示标签页，可动态配置标签页标题
+ *
+ * @param pathID 预定义的标签页路由，可据此动态配置标题
+ * @param predefinePathName 预定义的已转义的国际化标题
+ * @param params 路由中的参数
+ */
+function setPathName(pathID: string, predefinePathName: string, params: any) {
+  if (pathID.includes('dynamic')) {
+    return `${predefinePathName} - ${params.name}`;
+  }
+  return predefinePathName;
+}
+
+function getParams(path: string, pathname: string): { [key: string]: string } {
+  const match = pathToRegexp.match(path);
+  const result = match(pathname) as {
+    index: number;
+    params: { [k: string]: string };
+    path: string;
+  };
+  return result.params;
+}
+
 function routeTo(targetTab: ChildrenTab<{ location: any }>) {
   router.push(targetTab.extraTabProperties.location);
 }
 
 export interface PageTabsProps {
   proRootPath?: string;
+  pageTabs?: 'route' | 'path';
   children?: UmiChildren;
   originalMenuData: MenuItem[];
   location: RouteData;
 }
 
 function PageTabs(props: PageTabsProps) {
-  const { proRootPath = '/', children, originalMenuData, location } = props;
-
+  const { proRootPath = '/', pageTabs, children, originalMenuData, location } = props;
   /** return children to redirect if children pathname equal proRootPath */
   if (location.pathname === proRootPath) {
     return children;
   }
   const [pathID, pathName] = getMetadataOfTab(location.pathname, originalMenuData);
-
   const handleTabChange = (keyToSwitch: string, activedTabs: ChildrenTab[]) => {
     const targetTab = _find(activedTabs, { key: keyToSwitch });
     routeTo(targetTab);
@@ -61,16 +84,26 @@ function PageTabs(props: PageTabsProps) {
     routeTo(targetTab);
   };
 
+  const activeKey = pageTabs === 'path' ? location.pathname : pathID;
+  const activeTitle =
+    pageTabs === 'path'
+      ? setPathName(pathID, pathName, getParams(pathID, location.pathname))
+      : pathName;
   return (
-    <ChildrenTabs
-      activeKey={pathID}
-      activeTitle={pathName}
-      extraTabProperties={{ location }}
-      handleTabChange={handleTabChange}
-      afterRemoveTab={afterRemoveTab}
-    >
-      {children}
-    </ChildrenTabs>
+    <div style={{ paddingTop: 2 }}>
+      <ChildrenTabs
+        activeKey={activeKey}
+        activeTitle={activeTitle}
+        extraTabProperties={{ location }}
+        handleTabChange={handleTabChange}
+        afterRemoveTab={afterRemoveTab}
+        tabsConfig={{
+          animated: true,
+        }}
+      >
+        {children}
+      </ChildrenTabs>
+    </div>
   );
 }
 
