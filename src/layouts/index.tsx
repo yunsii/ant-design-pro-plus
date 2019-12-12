a54a63eda0801f09be37bec2f8a566c896e864e7
Yuns Xie <yuns.xie@qq.com>
2019-12-12 14:37:08 +0800

    fix: re-render always by use state to set context value

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,21 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FormConfigProvider } from 'antd-curd';
 import uploadFn, { getUrl, isUploadOk } from '@/services/upload';
 
 const GlobalLayout: React.FC<any> = props => {
-  return (
-    <FormConfigProvider
-      value={{
-        uploadFn,
-        getUrl,
-        isUploadOk,
-        commenExtra: {
-          picture: '自定义图片默认提示',
-        },
-      }}
-      {...props}
-    />
-  );
+  const [config] = useState({
+    uploadFn,
+    getUrl,
+    isUploadOk,
+    commenExtra: {
+      picture: '自定义图片默认提示',
+    },
+  });
+  return <FormConfigProvider value={config} {...props} />;
 };
 
 export default GlobalLayout;
