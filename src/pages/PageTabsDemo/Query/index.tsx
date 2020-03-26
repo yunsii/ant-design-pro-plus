26c51845af7076faacc6ca3ad287edbfafe1e567
zpr1g <yuns.xie@qq.com>
2020-03-26 23:35:28 +0800

    chore: update demo

--- src/pages/PageTabsDemo/Query/index.tsx
+++ src/pages/PageTabsDemo/Query/index.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
-import { Card, Input } from 'antd';
+import { Card, Input, Checkbox } from 'antd';
 import router from 'umi/router';
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
+import _find from 'lodash/find';
 
 export default () => {
   const [text, setText] = useState<string>();
+  const [options, setOptions] = useState<any[]>([]);
 
   return (
     <PageHeaderWrapper
@@ -17,8 +19,29 @@ export default () => {
           onChange={(e) => setText(e.target.value)}
           onPressEnter={() => {
             if (text?.trim()) {
-              router.push(`/page-tabs-demo/result/${text}`);
+              router.push({
+                pathname: `/page-tabs-demo/result/${text}`,
+                state: options.includes("withState") ? { state: "yes" } : undefined,
+                query: options.includes("withQuery") ? { query: "yes" } : undefined,
+              });
             }
+          }}
+        />
+        <Checkbox.Group
+          style={{ marginTop: 12 }}
+          value={options}
+          options={[
+            {
+              label: "with state (`{ state: \"yes\"}`)",
+              value: "withState",
+            },
+            {
+              label: "with query (`{ query: \"yes\"}`)",
+              value: "withQuery",
+            },
+          ]}
+          onChange={(_options) => {
+            setOptions(_options);
           }}
         />
       </Card>
--- src/pages/PageTabsDemo/Result/index.tsx
+++ src/pages/PageTabsDemo/Result/index.tsx
@@ -2,16 +2,20 @@ import React from 'react';
 import { Card } from 'antd';
 import { match as Match } from 'react-router';
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
+import { BeautifulLocation } from '@/components/PageTabs/data';
 
-export default ({ match }: { match: Match }) => {
-  console.log(match);
-
+export default ({ match, location }: { match: Match, location: BeautifulLocation }) => {
   return (
     <PageHeaderWrapper
       title="Result"
     >
       <Card title="Result">
-        {JSON.stringify(match)}
+        <p>
+          match: {JSON.stringify(match)}
+        </p>
+        <p>
+          location: {JSON.stringify(location)}
+        </p>
       </Card>
     </PageHeaderWrapper>
   )
