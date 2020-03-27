d4c6265df09701ed6a17865154571f7cf933bfc8
zpr1g <yuns.xie@qq.com>
2020-03-27 10:22:20 +0800

    feat: add type

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -11,6 +11,7 @@ declare module '*.gif';
 declare module '*.bmp';
 declare module '*.tiff';
 declare module 'omit.js';
+declare module 'svg';
 
 // google analytics interface
 interface GAFieldsObject {
