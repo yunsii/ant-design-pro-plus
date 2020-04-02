6c9648af03aea7cdd700da33d2f9d6216e03da10
zpr1g <yuns.xie@qq.com>
2020-04-02 12:32:21 +0800

    refactor: localeRoutes to use orignal routes

--- src/layouts/RouteTabsLayout/index.tsx
+++ src/layouts/RouteTabsLayout/index.tsx
@@ -6,17 +6,63 @@
 import { MenuDataItem } from '@ant-design/pro-layout';
 import React from 'react';
 import * as H from 'history';
+import { Route } from '@ant-design/pro-layout/lib/typings';
+import { formatMessage } from 'umi-plugin-react/locale';
+import _isArray from 'lodash/isArray';
+import memoizedOne from 'memoize-one';
+import deepEqual from 'fast-deep-equal';
 
 import { footerRender } from '@/layouts/BasicLayout';
 import RouteTabs from '@/components/RouteTabs';
 import { UmiChildren, RouteTabsMode } from '@/components/RouteTabs/data';
 import PageLoading from '@/components/PageLoading';
 
+/** 根据路由定义中的 name 本地化标题 */
+function localeRoutes(routes: Route[], parent: MenuDataItem | null = null): MenuDataItem[] {
+  const result: MenuDataItem[] = [];
+
+  routes.forEach(item => {
+    const { routes: itemRoutes, ...rest } = item;
+
+    if (!item.name) {
+      return;
+    }
+
+    // 初始化 locale 字段
+    let newItem: MenuDataItem = {
+      ...rest,
+      routes: null,
+      locale: item.name,
+    };
+
+    const localeId = parent ? `${parent.locale}.${newItem.locale}` : `menu.${newItem.locale}`;
+
+    newItem = {
+      ...rest,
+      locale: localeId,
+      name: formatMessage({ id: localeId }),
+    };
+
+    if (_isArray(itemRoutes) && itemRoutes.length) {
+      newItem = {
+        ...newItem,
+        children: localeRoutes(itemRoutes, newItem),
+      };
+    }
+
+    result.push(newItem);
+  });
+
+  return result;
+}
+
+const memoizedOneLocaleRoutes = memoizedOne(localeRoutes, deepEqual);
+
 export interface RouteTabsLayoutProps {
   mode?: RouteTabsMode | false;
   fixedPageTabs?: boolean;
   children: UmiChildren;
-  originalMenuData?: MenuDataItem[];
+  routes?: Route[];
   setTabTitle?: (
     path: string,
     locale: string,
@@ -31,7 +77,7 @@ function RouteTabsLayout(props: RouteTabsLayoutProps): JSX.Element {
     mode,
     fixedPageTabs,
     menuLoading,
-    originalMenuData,
+    routes,
 
     children,
   } = props;
@@ -39,12 +85,12 @@ function RouteTabsLayout(props: RouteTabsLayoutProps): JSX.Element {
   if (mode && menuLoading) {
     return <PageLoading />;
   }
-  if (mode && originalMenuData) {
+  if (mode && routes) {
     return (
       <RouteTabs
         mode={mode}
         fixedPageTabs={fixedPageTabs}
-        originalMenuData={originalMenuData}
+        originalMenuData={memoizedOneLocaleRoutes(routes)}
         // animated={false}
       >
         <div>
