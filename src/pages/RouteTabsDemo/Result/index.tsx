e55fe0fbad60e9ceab8975ea6b30365747a58cd0
zpr1g <yuns.xie@qq.com>
2020-04-19 23:54:24 +0800

    feat: withRouteTab

--- src/pages/RouteTabsDemo/Result/index.tsx
+++ src/pages/RouteTabsDemo/Result/index.tsx
@@ -4,17 +4,20 @@ import { match as Match } from 'react-router';
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
 
 import { BeautifulLocation } from '@/components/RouteTabs/data';
+import { withRouteTab } from '@/components/RouteTabs/utils';
 import { useConsole } from '@/hooks/test/lifeCycle';
 
-export default ({ match, location }: { match: Match; location: BeautifulLocation }) => {
-  useConsole('Result');
+export default withRouteTab(
+  ({ match, location }: { match: Match; location: BeautifulLocation }) => {
+    useConsole('Result');
 
-  return (
-    <PageHeaderWrapper title='Result'>
-      <Card title='Result'>
-        <p>match: {JSON.stringify(match)}</p>
-        <p>location: {JSON.stringify(location)}</p>
-      </Card>
-    </PageHeaderWrapper>
-  );
-};
+    return (
+      <PageHeaderWrapper title='Result'>
+        <Card title='Result'>
+          <p>match: {JSON.stringify(match)}</p>
+          <p>location: {JSON.stringify(location)}</p>
+        </Card>
+      </PageHeaderWrapper>
+    );
+  },
+);
