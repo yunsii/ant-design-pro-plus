f50dee5d99aaf84ed74a1da334bf426b4e74b2d6
zpr1g <yuns.xie@qq.com>
2020-04-19 23:22:23 +0800

    chore: useConsole

--- src/pages/RouteTabsDemo/Result/index.tsx
+++ src/pages/RouteTabsDemo/Result/index.tsx
@@ -2,23 +2,19 @@ import React from 'react';
 import { Card } from 'antd';
 import { match as Match } from 'react-router';
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
-import { BeautifulLocation } from '@/components/RouteTabs/data';
 
-export default ({ match, location }: { match: Match, location: BeautifulLocation }) => {
-  console.log("render Result");
+import { BeautifulLocation } from '@/components/RouteTabs/data';
+import { useConsole } from '@/hooks/test/lifeCycle';
+
+export default ({ match, location }: { match: Match; location: BeautifulLocation }) => {
+  useConsole('Result');
 
   return (
-    <PageHeaderWrapper
-      title="Result"
-    >
-      <Card title="Result">
-        <p>
-          match: {JSON.stringify(match)}
-        </p>
-        <p>
-          location: {JSON.stringify(location)}
-        </p>
+    <PageHeaderWrapper title='Result'>
+      <Card title='Result'>
+        <p>match: {JSON.stringify(match)}</p>
+        <p>location: {JSON.stringify(location)}</p>
       </Card>
     </PageHeaderWrapper>
-  )
-}
+  );
+};
