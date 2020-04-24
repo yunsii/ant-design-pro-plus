7d4efa9e08ad01e17c205642bd6d4cc859fb7a45
zpr1g <yuns.xie@qq.com>
2020-04-24 15:22:30 +0800

    fix: update function name

--- src/components/GlobalHeader/RightContent.tsx
+++ src/components/GlobalHeader/RightContent.tsx
@@ -56,7 +56,7 @@ const GlobalHeaderRight: React.SFC<GlobalHeaderRightProps> = props => {
       />
       {routeTabsMode && reloadTab ? (
         <Tooltip title={formatMessage({ id: 'component.globalHeader.reload' })}>
-          <a className={styles.action} onClick={() => window.reloadCurrentTab()}>
+          <a className={styles.action} onClick={() => window.reloadTab()}>
             <ReloadOutlined />
           </a>
         </Tooltip>
