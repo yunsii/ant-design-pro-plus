88b7723981a3469638b91153d74061a5ee117597
zpr1g <yuns.xie@qq.com>
2020-04-25 23:34:13 +0800

    feat: "no-unused-expressions": "off"

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
   extends: [require.resolve('@umijs/fabric/dist/eslint')],
+  rules: { 'no-unused-expressions': 'off' },
   globals: {
     ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION: true,
     page: true,
