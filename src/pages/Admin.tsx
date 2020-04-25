e987b1c4ba41e72311c85c179763a2596df40b51
zpr1g <yuns.xie@qq.com>
2020-04-25 23:22:06 +0800

    chore: update demo

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { HeartTwoTone, SmileTwoTone } from '@ant-design/icons';
-import { Card, Typography, Alert } from 'antd';
+import { Card, Typography, Alert, Button } from 'antd';
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
 
 export default (): React.ReactNode => (
-  <PageHeaderWrapper content=" 这个页面只有 admin 权限才能查看">
+  <PageHeaderWrapper content=' 这个页面只有 admin 权限才能查看'>
     <Card>
       <Alert
-        message="umi ui 现已发布，欢迎使用 npm run ui 启动体验。"
-        type="success"
+        message='umi ui 现已发布，欢迎使用 npm run ui 启动体验。'
+        type='success'
         showIcon
         banner
         style={{
@@ -17,12 +17,20 @@ export default (): React.ReactNode => (
         }}
       />
       <Typography.Title level={2} style={{ textAlign: 'center' }}>
-        <SmileTwoTone /> Ant Design Pro <HeartTwoTone twoToneColor="#eb2f96" /> You
+        <SmileTwoTone /> Ant Design Pro <HeartTwoTone twoToneColor='#eb2f96' /> You
       </Typography.Title>
+      <Button
+        type='primary'
+        onClick={() => {
+          window.closeAndGoBackTab('/list', () => window.reloadTab('/list'));
+        }}
+      >
+        Close and Go Back Tab
+      </Button>
     </Card>
     <p style={{ textAlign: 'center', marginTop: 24 }}>
       Want to add more pages? Please refer to{' '}
-      <a href="https://pro.ant.design/docs/block-cn" target="_blank" rel="noopener noreferrer">
+      <a href='https://pro.ant.design/docs/block-cn' target='_blank' rel='noopener noreferrer'>
         use block
       </a>
       。
