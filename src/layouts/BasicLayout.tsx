b8770b357dab511147c24e4e4744610e0ea1e835
zpr1g <yuns.xie@qq.com>
2020-06-17 22:56:07 +0800

    chore: code format

--- src/layouts/BasicLayout.tsx
+++ src/layouts/BasicLayout.tsx
@@ -33,11 +33,11 @@ import styles from './BasicLayout.less';
 const noMatch = (
   <Result
     status={403}
-    title="403"
-    subTitle="Sorry, you are not authorized to access this page."
+    title='403'
+    subTitle='Sorry, you are not authorized to access this page.'
     extra={
-      <Button type="primary">
-        <Link to="/user/login">Go Login</Link>
+      <Button type='primary'>
+        <Link to='/user/login'>Go Login</Link>
       </Button>
     }
   />
@@ -74,7 +74,7 @@ const menuDataRender = (menuList: MenuDataItem[]): MenuDataItem[] =>
 
 const defaultFooterDom = (
   <DefaultFooter
-    copyright="2019 蚂蚁金服体验技术部出品"
+    copyright='2019 蚂蚁金服体验技术部出品'
     links={[
       {
         key: 'Ant Design Pro',
@@ -112,11 +112,11 @@ export const footerRender = () => {
           textAlign: 'center',
         }}
       >
-        <a href="https://www.netlify.com" target="_blank" rel="noopener noreferrer">
+        <a href='https://www.netlify.com' target='_blank' rel='noopener noreferrer'>
           <img
-            src="https://www.netlify.com/img/global/badges/netlify-color-bg.svg"
-            width="82px"
-            alt="netlify logo"
+            src='https://www.netlify.com/img/global/badges/netlify-color-bg.svg'
+            width='82px'
+            alt='netlify logo'
           />
         </a>
       </div>
@@ -191,7 +191,7 @@ const BasicLayout: React.FC<BasicLayoutProps> = props => {
         )
       }
       menuHeaderRender={(logoDom, titleDom) => (
-        <Link to="/">
+        <Link to='/'>
           {logoDom}
           {titleDom}
         </Link>
