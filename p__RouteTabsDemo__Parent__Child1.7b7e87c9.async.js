fe3a4613a5b4bc7bf57588bc28921716a546dc51
theprimone <theprimone@users.noreply.github.com>
2021-02-25 12:58:53 +0000

    deploy: 68a40fd150094eaa4e08c92aa42cc16c371f4206

---
+++ p__RouteTabsDemo__Parent__Child1.7b7e87c9.async.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([[12],{B4tI:function(n,t,e){"use strict";e.r(t);var c=e("q1tI"),i=e.n(c);t["default"]=function(){return i.a.createElement("div",null,"child 1")}}}]);
---
+++ p__RouteTabsDemo__Parent__Child2.18a07746.async.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([[13],{liBR:function(n,e,t){"use strict";t.r(e);var i=t("q1tI"),c=t.n(i);e["default"]=function(){return c.a.createElement("div",null,"child 2")}}}]);
