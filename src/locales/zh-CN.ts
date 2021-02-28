66e7b3f5d9db5a5b55564081b4c9b56a17b48bb5
themprimone <yuns.xie@qq.com>
2021-02-28 11:43:50 +0800

    fix: intl warning

--- src/locales/zh-CN.ts
+++ src/locales/zh-CN.ts
@@ -4,6 +4,7 @@ import menu from './zh-CN/menu';
 import pwa from './zh-CN/pwa';
 import settingDrawer from './zh-CN/settingDrawer';
 import settings from './zh-CN/settings';
+import pages from './zh-CN/pages';
 
 export default {
   'navBar.lang': '语言',
@@ -19,4 +20,5 @@ export default {
   ...settings,
   ...pwa,
   ...component,
+  ...pages,
 };
