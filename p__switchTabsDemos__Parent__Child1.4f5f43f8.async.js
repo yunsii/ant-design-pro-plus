357485c0cfbf3494283f125c2ffbe87791b77b1e
theprimone <theprimone@users.noreply.github.com>
2021-11-23 14:59:50 +0000

    deploy: 6b64d4e2aeb7455c6d7ec67620af487d262498db

---
+++ p__switchTabsDemos__Parent__Child1.4f5f43f8.async.js
@@ -0,0 +1,1 @@
+(self.webpackChunkant_design_pro=self.webpackChunkant_design_pro||[]).push([[528],{89645:function(s,n,u){"use strict";u.r(n);var e=u(85893);n.default=function(){return(0,e.jsx)("div",{children:"child 1"})}}}]);
