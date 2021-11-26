4d896428d0adedb63cc54c8898733143371603b8
themprimone <yuns.xie@qq.com>
2021-11-26 17:40:37 +0800

    chore: add page container

--- src/pages/switchTabsDemos/Dynamic/index.tsx
+++ src/pages/switchTabsDemos/Dynamic/index.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
 import { useParams } from 'umi';
 import { Card } from 'antd';
+import { PageHeaderWrapper } from '@ant-design/pro-layout';
 
 export default function Dynamic() {
   const params = useParams();
 
   return (
-    <Card>
-      match params:
-      <div style={{ whiteSpace: 'pre-wrap' }}>{JSON.stringify(params, null, 2)}</div>
-    </Card>
+    <PageHeaderWrapper content="动态路由">
+      <Card>
+        match params:
+        <div style={{ whiteSpace: 'pre-wrap' }}>{JSON.stringify(params, null, 2)}</div>
+      </Card>
+    </PageHeaderWrapper>
   );
 }
