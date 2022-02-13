2fd88544e8f6ca5b3d2b4a84ffd2db74e717100a
yunsii <yunsii@users.noreply.github.com>
2022-02-13 13:20:14 +0000

    deploy: 21ddace5cbec558b970c6da3006d9f07fbd538d9

---
+++ p__SwitchTabsDemos__Parent__Child2.f33bc147.async.js
@@ -0,0 +1,1 @@
+(self.webpackChunkant_design_pro=self.webpackChunkant_design_pro||[]).push([[582],{40665:function(s,n,u){"use strict";u.r(n);var e=u(85893);n.default=function(){return(0,e.jsx)("div",{children:"child 2"})}}}]);
--- p__switchTabsDemos__Parent__Child2.8af7afa5.async.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkant_design_pro=self.webpackChunkant_design_pro||[]).push([[945],{7903:function(s,n,u){"use strict";u.r(n);var e=u(85893);n.default=function(){return(0,e.jsx)("div",{children:"child 2"})}}}]);
